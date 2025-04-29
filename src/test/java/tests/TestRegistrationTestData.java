d9bafc10e8ddf702ee6479cce7f8c898b0651dc3
Marie Ch <zima04@mail.ru>
2025-04-29 15:02:35 +0300

    правка проверка результатов

--- src/test/java/tests/TestRegistrationTestData.java
+++ src/test/java/tests/TestRegistrationTestData.java
@@ -49,7 +49,6 @@ public class TestRegistrationTestData extends TestDemoqa {
                 .setPhone(testData.userNumber)
                 .selectGender(testData.gender)
                 .submitForm();
-
         registrationFormPage.shouldAppear()
                 .checkResult("Student Name", testData.firstName + " " + testData.lastName)
                 .checkResult("Gender", testData.gender)
