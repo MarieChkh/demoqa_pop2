9ca2fbad56805c90dbf8594e910d1da072de7d3d
Marie Ch <zima04@mail.ru>
2025-05-11 01:34:22 +0300

    12 урок домашнее задание

--- src/test/java/tests/demoqa/TestDemoqa.java
+++ src/test/java/tests/demoqa/TestDemoqa.java
@@ -2,6 +2,7 @@ package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -18,6 +19,7 @@ public class TestDemoqa {
     }
 
     @Test
+    @Tag("smoke")
     void fillFormTest() {
 
         open("/automation-practice-form");
